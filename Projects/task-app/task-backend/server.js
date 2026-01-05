8a4a6e80f33b2d1864ed2ab05264b02b115e21b7
raut-abhishek <iamabhishekraut01@gmail.com>
2026-01-05 13:34:23 +0530

    db connection

--- Projects/task-app/task-backend/server.js
+++ Projects/task-app/task-backend/server.js
@@ -8,3 +8,20 @@ const app = express();
 app.use(cors());
 
 app.use(express.json());
+
+
+const db = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'Pass@123',
+    database:'task_app'
+});
+
+
+db.connect(err =>{
+    if(err){
+        console.log('Database connection error:', err);
+    }else{
+        console.log('Connected to the database');
+    };
+})
